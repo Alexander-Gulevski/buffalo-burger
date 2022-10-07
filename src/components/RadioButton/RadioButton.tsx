8ed44efbfa2712f19eafda981aea47a473da3d19
Alexander Gulevski <gulevski007@mail.ru>
2022-10-07 22:36:15 +0300

    add borfer radio-button and add colors

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import Text from "../Text/Text";
 import { StyledRadioButton } from "./styled";
 
 const RadioButton = () => {
+  const [isClick, setIsClick] = useState(false);
 
+  const handleClick = () => {
+    setIsClick(!isClick);
+  };
   return (
-    <StyledRadioButton><input
-        type="checkbox"
-        id='1'
-      />
-     <label htmlFor="1"><Text/></label>
+    <StyledRadioButton onClick={handleClick} handleClick={isClick}>
+      <input type="checkbox" id="1" />
+      <label htmlFor="1">
+        <Text />
+      </label>
     </StyledRadioButton>
-    
   );
-}
+};
 
 export default RadioButton;
